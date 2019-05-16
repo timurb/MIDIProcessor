9009fc5fb03e1fedc904f14c6585f81279f56775
Timur Batyrshin <erthad@gmail.com>
2019-05-16 23:25:13 +0300

    update mapping script to skip system devices

--- scripts/client-mapping.py
+++ scripts/client-mapping.py
@@ -9,7 +9,10 @@ for line in sys.stdin.readlines():
     if re.match("^client", line):
         id = line.split()[1].replace(':','')
         if id == '0': continue
+
         if id == sys.argv[1]:
             print(current_number)
             exit()
+        current_number += 1
+    elif id != '0' and re.match("^    [1-9]", line):
         current_number += 1
