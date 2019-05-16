4e1bf5b3d6c7122ed563110983a038ae435e2921
Timur Batyrshin <erthad@gmail.com>
2019-05-16 23:24:54 +0300

    disable sound processing

--- scripts/start-processing.sh
+++ scripts/start-processing.sh
@@ -6,7 +6,7 @@ OUTDEVICE="$(aconnect -l | scripts/client-mapping.py 20)"	# Xio
 KEYBOARD="$(aconnect -l | scripts/client-mapping.py 24)"	# Arturia Keystep
 CONTROLLER="$(aconnect -l | scripts/client-mapping.py 28)"	# Launch Control XL
 
-chuck classes/* \
+chuck --silent classes/* \
     "process_notes.ck:${KEYBOARD}:${OUTDEVICE}" \
     "process_nomod.ck:${KEYBOARD}:${OUTDEVICE}" \
     "process_cc.ck:${CONTROLLER}:${OUTDEVICE}"
