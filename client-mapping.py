e0d4e75d2c9acbd9a1bf0e612a8a235c390d9581
Timur Batyrshin <erthad@gmail.com>
2019-05-16 13:30:29 +0300

    add script for mapping aconnect -l output to chuck midi ports

---
+++ client-mapping.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys
+import re
+
+current_number = 0
+
+for line in sys.stdin.readlines():
+    if re.match("^client", line):
+        id = line.split()[1].replace(':','')
+        if id == '0': continue
+        if id == sys.argv[1]:
+            print(current_number)
+            exit()
+        current_number += 1
