d8cb4345d5dbeb15e8b8d422f694734ddd529ca3
Timur Batyrshin <erthad@gmail.com>
2019-05-16 15:21:57 +0300

    add script to start ChucK

---
+++ scripts/start-processing.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd "$(dirname $0)/.."
+
+OUTDEVICE="$(aconnect -l | scripts/client-mapping.py 20)"	# Xio
+KEYBOARD="$(aconnect -l | scripts/client-mapping.py 24)"	# Arturia Keystep
+CONTROLLER="$(aconnect -l | scripts/client-mapping.py 28)"	# Launch Control XL
+
+chuck classes/* \
+    "process_notes.ck:${KEYBOARD}:${OUTDEVICE}" \
+    "process_nomod.ck:${KEYBOARD}:${OUTDEVICE}" \
+    "process_cc.ck:${CONTROLLE}:${OUTDEVICE}"
